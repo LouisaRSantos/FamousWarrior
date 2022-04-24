0edf346869477f199a95b5711296923051b0db36
Sier Santos <louisasantosl12@gmail.com>
2022-04-24 17:23:19 +0900

    I have added a new feature called OOP_Assignment1

---
+++ trytry/Form1.cs
@@ -0,0 +1,45 @@
+namespace trytry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I'm Louisa Reighn Alejo Santos");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Good Day, Sir Danilo!");
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("People call me 'Isay' or 'Sier'.");
+        }
+    }
+}
